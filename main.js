26cdee37ada9d58b33e9d5323b6636663beb867f
rayangler <rayangler@users.noreply.github.com>
2017-12-31 00:34:41 -0500

    Update main.js

--- main.js
+++ main.js
@@ -9,11 +9,15 @@ client.on('message', message => {
   var input = message.content.toLowerCase();
   
   if (input == "badabing" ||
-      input == "bada bing") {
+      input == "bada bing" ||
+      input == ":b:ada:b:ing" ||
+      input == ":b:ada :b:ing") {
     message.channel.send(":b:ada:b:oom");
   }
   if (input == "badaboom" ||
-      input == "bada boom") {
+      input == "bada boom" ||
+      input == ":b:ada:b:oom" ||
+      input == ":b:ada :b:oom") {
     message.channel.send(":b:ada:b:ing");
   }
   if (input == "badabot creator") {
