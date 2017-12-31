0a31c8b1ba763a3efa4c5e120b79f63cf17c8f4c
rayangler <rayangler@users.noreply.github.com>
2017-12-31 01:03:46 -0500

    Update main.js

--- main.js
+++ main.js
@@ -8,16 +8,27 @@ client.on('ready', () => {
 client.on('message', message => {
   var input = message.content.toLowerCase();
   
-  if (input == "badabing" ||
-      input == "bada bing") {
+  // Basic Bada commands
+  if (input.includes("badabing") ||
+      input.includes("bada bing") ) {
     message.channel.send(":b:ada:b:oom");
   }
-  if (input == "badaboom" ||
-      input == "bada boom") {
+  if (input.includes("badaboom") ||
+      input.includes("bada boom") ) {
     message.channel.send(":b:ada:b:ing");
   }
+  if (input.includes("badabig") ||
+      input.includes("bada big") ) {
+    message.channel.send(":b:adaNasty");
+  }
+  if (input.includes("how's badabusiness") ||
+      input.includes("hows badabusiness") ) {
+    message.channel.send(":b:ada:b:oomin'"
+  }
+  
+  // Misc. Commands
   if (input == "badabot creator") {
-    message.channel.send("My creator is none other than Raymund. You're welcome.");
+    message.channel.send("My creator is none other than :b:aemund. You're welcome.");
   }
   
 });
