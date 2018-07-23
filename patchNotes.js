01ead2cda2c6e684470bc5c58db0b30e6aba7d2f
unknown <rangetzu8808@gmail.com>
2018-07-22 23:09:17 -0400

    Fix patch notes

--- patchNotes.js
+++ patchNotes.js
@@ -1,11 +1,11 @@
 function patch1_1() {
   return ("```md\n\
-    /* BADABOT 1.1 BOOSTER PACK PATCH NOTES: */\n\
-    + Added <badabam> command.\n\
-    + Added <badabigballerbrand> command.\n\
-    + Added <producer> command.\n\
-    + <Yer>? Yyyyyeeeeerrrrrr.\n\
-    ```");
+/* BADABOT 1.1 BOOSTER PACK PATCH NOTES: */\n\
++ Added <badabam> command.\n\
++ Added <badabigballerbrand> command.\n\
++ Added <producer> command.\n\
++ <Yer>? Yyyyyeeeeerrrrrr.\n\
+```");
 }
 
 module.exports = {
