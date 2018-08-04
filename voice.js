b6be6110d84a5fad9ba8e4c27f529d1519ed0f84
Raymund Rodriguez <raymundrodriguez@HON-2016355.home>
2018-08-04 11:29:23 -0400

    Update voice.js

--- voice.js
+++ voice.js
@@ -3,16 +3,27 @@ const Discord = require('discord.js');
 var voice = "uk-male";
 
 function chooseVoiceFile(command) {
-
+  
 }
 
 module.exports = {
   playCommand: function(voiceChannel, command) {
     voiceChannel.join()
       .then(connection => {
-        console.log('Connected');
+        console.log('Connected to voice channel: ' + voiceChannel);
         const dispatcher = connection.playFile('./test.mp3');
       })
       .catch(console.error);
+  },
+
+  leave: function(voiceChannel) {
+    console.log('Disconnected from voice channel: ' + voiceChannel);
+    voiceChannel.leave();
+  },
+
+  changeVoice(newVoice) {
+    if (newVoice == voice) return;
+    voice = newVoice;
+    console.log("Voice changed to: " + voice);
   }
 };
